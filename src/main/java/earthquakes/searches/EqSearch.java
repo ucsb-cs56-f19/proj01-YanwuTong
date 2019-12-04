80e9fe1c0628d782779a7bc4428fc7252123ec3f
y_tong@umail.ucsb.edu <y_tong@umail.ucsb.edu>
2019-12-04 12:58:06 -0800

    yt - add new search selection but not w work yet

--- src/main/java/earthquakes/searches/EqSearch.java
+++ src/main/java/earthquakes/searches/EqSearch.java
@@ -4,6 +4,9 @@ public class EqSearch{
 
     private int distance;
     private int minmag;
+    private long lat;
+    private long lon;
+    private String location;
 
     public int getDistance(){
 	return this.distance;
@@ -11,6 +14,19 @@ public class EqSearch{
     
     public int getMinmag(){
 	return this.minmag;
+    }
+
+
+    public long getLat(){
+        return this.lat;
+    }
+
+    public long getlon(){
+        return this.lon;
+    }
+
+    public String getLocation(){
+        return this.location;
     }
 
     public EqSearch(){
@@ -24,5 +40,17 @@ public class EqSearch{
     
     public void setMinmag(int min){
 	this.minmag = min;
+    }
+
+    public void setLat(long lat){
+        this.lat = lat;
+    }
+
+    public void setLon(long lon){
+        this.lon = lon;
+    }
+
+    public void setLocation(String location){
+        this.location = location;
     }
 }
